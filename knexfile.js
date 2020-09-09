00bb55fd3e84c955431c21cc2026bb89e937b76b
Braden Bell <fish0859@gmail.com>
2020-09-08 20:58:27 -0600

    comments

--- knexfile.js
+++ knexfile.js
@@ -48,20 +48,31 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './database.sqlite3'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    },
+  },
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 };
